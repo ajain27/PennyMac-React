461b5b5c99dbc4a9fb454242b4e52993666e7c81
Ankit Jain <ankit.jain67@t-mobile.com>
2021-01-07 11:42:38 -0800

    Fixing the use effect hook

--- src/components/movieDetail.jsx
+++ src/components/movieDetail.jsx
@@ -10,35 +10,37 @@ function MovieDetail({ match }) {
 
     const url = `http://api.tvmaze.com/shows/${match.params.id}/episodes`;
 
+
+
     useEffect(() => {
+        const fetchEpisodes = async () => {
+            axios.get(url).then(res => {
+                setLoading(false);
+                const episodes = res.data;
+                setEpisodes(episodes);
+                setError('');
+
+            }).catch(error => {
+                setLoading(false);
+                setEpisodes([]);
+                setError('Something went wrong, cannot load the episodes');
+            })
+        };
         fetchEpisodes();
-    }, [])
+    }, [url])
 
-    const fetchEpisodes = async () => {
-        axios.get(url).then(res => {
-            setLoading(false);
-            const episodes = res.data;
-            console.log('Episodes--', episodes);
-            setEpisodes(episodes);
-            setError('');
 
-        }).catch(error => {
-            setLoading(false);
-            setEpisodes([]);
-            setError('Something went wrong, cannot load the episodes');
-        })
-    };
     return (
         <div className="detail-wrapper">
             <div className="custom-container">
                 <div className="list-container">
                     <div className="row">
-                        <div className="col-sm-5 p-0" style={{postion: "relative", top: "25px"}}>
+                        <div className="col-sm-5 p-0" style={{ postion: "relative", top: "25px" }}>
                             <img src="https://picsum.photos/200/300" alt="" className="detail-image" />
-                            <p style={{margin: "10px"}}>lavishly illustrated with detailed examples, The Principles of Beautiful Web Design is an easy-to-follow guide that will lead you through the process of creating great designs from start to finish. No prior web design experience is required.</p>
+                            <p style={{ margin: "10px" }}>lavishly illustrated with detailed examples, The Principles of Beautiful Web Design is an easy-to-follow guide that will lead you through the process of creating great designs from start to finish. No prior web design experience is required.</p>
                         </div>
                         <div className='col-sm-7 p-0'>
-                            <div style={{position: "relative", top: "25px"}}>
+                            <div style={{ position: "relative", top: "25px" }}>
                                 <select class="custom-select custom-select-lg select-season mb-3">
                                     <option>Season 1</option>
                                 </select>
@@ -53,9 +55,9 @@ function MovieDetail({ match }) {
                                                                 {episodes && episodes.number && episodes.number < 10 ? '0' + episodes.number : episodes.number}
                                                             </div>
                                                         </div>
-                                                        <div className="col-sm-10 p-0" style={{"line-height": "0px"}}>
+                                                        <div className="col-sm-10 p-0" style={{ "line-height": "0px" }}>
                                                             <p className="episode-name"><strong>{episodes.name}</strong></p>
-                                                            <BsStar className="rating" style={{"line-height": "0px"}} />
+                                                            <BsStar className="rating" style={{ "line-height": "0px" }} />
                                                             <p className="episode-airdate">{episodes.airdate}</p>
                                                         </div>
                                                     </div>
